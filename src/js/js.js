2414512ff55629131177e8ed4c2bd7766094726b
Ismayil <hajiev93@gmail.com>
2020-06-08 10:57:37 +0300

    feedback ready

--- src/js/js.js
+++ src/js/js.js
@@ -4,6 +4,18 @@ $(function () {
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-    adaptiveHeight: true,
+  });
+});
+
+$(document).ready(function () {
+  $('.toggle-text').slideToggle(0); // скрывает текст
+  $('.about-toggle_show').click(function () {
+    $(this).prev('.toggle-text').slideToggle(200);
+    if ($(this).hasClass('active')) {
+      $(this).html('Свернуть');
+    } else {
+      $(this).html('Читать дальше');
+    }
+    $(this).toggleClass('active');
   });
 });
