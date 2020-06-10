7ae9ba9faca7fd506f060a9c0c96fdafc5aba908
marina_bee <marinamelihova29@gmail.com>
2020-06-10 08:06:06 +0300

    init scroll

--- src/js/js.js
+++ src/js/js.js
@@ -5,6 +5,9 @@ $(function () {
     speed: 300,
     slidesToShow: 1,
   });
+  $('section').addClass('wow fadeIn');
+  $('section').attr('data-wow-delay', '50ms');
+  $('section').attr('data-wow-duration', '1500ms');
 });
 
 $(document).ready(function () {
@@ -18,4 +21,20 @@ $(document).ready(function () {
     }
     $(this).toggleClass('active');
   });
+  $('a[href*="#"]').click(function () {
+    var $target = $(this.hash);
+    // if the section exists
+    if ($target.length) {
+      var targetOffset = $target.offset().top;
+      $('html,body').animate(
+        {
+          scrollTop: targetOffset,
+        },
+        2000,
+      );
+      return false;
+    }
+  });
 });
+
+new WOW().init();
