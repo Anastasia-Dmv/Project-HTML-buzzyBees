bed7f24ea459dec55344958c4c8135e4ef0bc98d
Anastasia-Dmv <Vesta50000@gmail.com>
2020-06-11 14:44:55 +0300

    add

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -17,7 +17,7 @@ const html = () => {
   return gulp
     .src(paths.src.html)
     .pipe(rigger())
-    .pipe(mode.production(htmlmin(htmlminConfig)))
+    // .pipe(mode.production(htmlmin(htmlminConfig)))
     .pipe(mode.production(cachebust(cachebustConfig)))
     .pipe(gulp.dest(paths.build.html));
 };
